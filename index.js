0f85343f963571ee260fc84376db085d8ca79de5
Varis <manjarb4@gmail.com>
2018-05-04 00:26:15 +0800

    Update google and passport key

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
+passport.use(new GoogleStrategy());
+
+/*app.get('/', (req, res) => {
     res.send({ bye: 'buddy'});
-});
+});*/
 
 const PORT = process.env.PORT || 5000;
 
