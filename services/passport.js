9da009ee6309b685d4791dbb3fd094a446b0f290
Varis <manjarb4@gmail.com>
2018-05-06 00:35:48 +0800

    Add create user

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy({
@@ -8,8 +11,11 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken, 'accessToken');
+        /*console.log(accessToken, 'accessToken');
         console.log(refreshToken, 'refreshToken');
-        console.log(profile, 'profile');
+        console.log(profile, 'profile');*/
+        new User({
+           googleId: profile.id
+        }).save();
     })
 );
